1283c795b7b48506fd5b19da6537b7cfb25a1403
Adriano Xavier <aj-dx@hotmail.com>
2022-01-21 16:46:26 -0300

    Aplicando modificações em App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import UsuarioEntrada from './components/UsuarioEntrada/UsuarioEntrada';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>Tarefa Componentes REACT</h1>
+        <UsuarioEntrada/>
+        <footer>
+          Desenvolvido por: Adriano Xavier
+        </footer>
+      </center>
     </div>
   );
 }
